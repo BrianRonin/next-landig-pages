231b4efcdbef6d7f1990fa6e59322021327e11b1
BrianRonin <01100010v@gmail.com>
2022-09-19 23:46:31 -0300

    up

--- src/api/load-pages.test.js
+++ src/api/load-pages.test.js
@@ -1,20 +1,42 @@
-import { loadPages } from './load-pages'
+// import { loadPages } from './load-pages'
+// import * as mapDataModule from './map-data'
 
-describe('Load-pages', () => {
-  let mockFetch = null
-  let mockJson = null
+// jest.mock('./map-data.js', () => {
+//   return {
+//     mapData: jest.fn().mockResolvedValue(Promise.resolve(['f'])),
+//   }
+// })
 
-  beforeEach(() => {
-    global.fetch = jest.fn()
-    mockJson = jest.fn().mockResolvedValue(Promise.resolve({ toJson: 1 }))
-    mockFetch = global.fetch
-    mockFetch.mockResolvedValue({
-      json: mockJson,
-    })
-  })
+// describe('Load-pages', () => {
+//   let mockFetch = null
+//   let mockJson = null
 
-  it('should return default page', () => {
-    const page = loadPages()
-    expect(mockFetch).toHaveBeenCalledTimes(1)
-  })
-})
+//   beforeEach(() => {
+//     global.process.env = {
+//       API_URL: 'api_url',
+//       API_SLUG: 'api_slug',
+//       API_ALL: 'api_all',
+//     }
+
+//     global.fetch = jest.fn()
+//     mockJson = jest.fn().mockResolvedValue(Promise.resolve([{ toJson: 1 }]))
+//     mockFetch = global.fetch
+//     mockFetch.mockResolvedValue({
+//       json: mockJson,
+//     })
+//   })
+
+//   afterAll(() => {
+//     jest.clearAllMocks()
+//   })
+
+//   it('should call default', async () => {
+//     loadPages()
+//     expect(mockFetch).toHaveBeenCalledTimes(1)
+//     expect(mockFetch).toHaveBeenCalledWith(process.env.API_SLUG + 'minha')
+//     expect(mockJson).toHaveBeenCalledTimes(1)
+//     expect(mapDataModule.mapData).toHaveBeenCalledTimes(1)
+//     expect(mapDataModule.mapData).toHaveBeenCalledWith([{ toJson: 1 }])
+//     expect(mapDataModule.mapData).toEqual({ mapped: 1 })
+//   })
+// })
